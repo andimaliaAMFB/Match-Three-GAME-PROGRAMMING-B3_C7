52e521df23d691855572dee9ca7248a52b91ceb5
Andi_malia010710 <011007andimalia@gmail.com>
2020-09-23 13:15:33 +0700

    Materi

---
+++ Membuat Game Match Three/Assets/Script/AchievementSystem.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AchievementSystem : Observer
+{
+    public Image achievementBanner;
+    public Text achievementText;
+
+    //Event
+    TileEvent cookiesEvent, cakeEvent, candyEvent;
+
+    void Start()
+    {
+        PlayerPrefs.DeleteAll();
+
+        //Buat event
+        cookiesEvent = new CookiesTileEvent(3);
+        cakeEvent = new CakeTileEvent(10);
+        candyEvent = new CandyTileEvent(5);
+
+        foreach (var poi in FindObjectsOfType<PointOfInterest>())
+        {
+            poi.RegisterObserver(this);
+        }
+    }
+
+    public override void OnNotify(string value)
+    {
+        string key;
+
+        //Seleksi event yang terjadi, dan panggil class event nya
+        if (value.Equals("Cookies event"))
+        {
+            cookiesEvent.OnMatch();
+            if (cookiesEvent.AchievementCompleted())
+            {
+                key = "Match first cookies";
+                NotifyAchievement(key, value);
+            }
+        }
+
+        if (value.Equals("Cake event"))
+        {
+            cakeEvent.OnMatch();
+            if (cakeEvent.AchievementCompleted())
+            {
+                key = "Match 10 cake";
+                NotifyAchievement(key, value);
+            }
+        }
+
+        if (value.Equals("Candy event"))
+        {
+
+            candyEvent.OnMatch();
+            if (candyEvent.AchievementCompleted())
+            {
+                key = "Match 5 cady";
+                NotifyAchievement(key, value);
+            }
+        }
+    }
+
+    void NotifyAchievement(string key, string value)
+    {
+        //check jika achievement sudah diperoleh
+        if (PlayerPrefs.GetInt(value) == 1)
+            return;
+
+        PlayerPrefs.SetInt(value, 1);
+        achievementText.text = key + " Unlocked !";
+
+        //pop up notifikasi
+        StartCoroutine(ShowAchievementBanner());
+    }
+
+    void ActivateAchievementBanner(bool active)
+    {
+        achievementBanner.gameObject.SetActive(active);
+    }
+
+    IEnumerator ShowAchievementBanner()
+    {
+        ActivateAchievementBanner(true);
+        yield return new WaitForSeconds(2f);
+        ActivateAchievementBanner(false);
+    }
+
+}
+
+public class CookiesTileEvent : TileEvent
+{
+    private int matchCount;
+    private int requiredAmount;
+
+    public CookiesTileEvent(int amount)
+    {
+        requiredAmount = amount;
+    }
+
+    public override void OnMatch()
+    {
+        matchCount++;
+    }
+
+    public override bool AchievementCompleted()
+    {
+        if (matchCount == requiredAmount)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+public class CakeTileEvent : TileEvent
+{
+    private int matchCount;
+    private int requiredAmount;
+
+    public CakeTileEvent(int amount)
+    {
+        requiredAmount = amount;
+    }
+
+    public override void OnMatch()
+    {
+        matchCount++;
+    }
+
+    public override bool AchievementCompleted()
+    {
+        if (matchCount == requiredAmount)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+public class CandyTileEvent : TileEvent
+{
+    private int matchCount;
+    private int requiredAmount;
+
+    public CandyTileEvent(int amount)
+    {
+        requiredAmount = amount;
+    }
+
+    public override void OnMatch()
+    {
+        matchCount++;
+    }
+
+    public override bool AchievementCompleted()
+    {
+        if (matchCount == requiredAmount)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Membuat Game Match Three/Assets/Script/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //Instance sebagai global access
+    public static GameManager instance;
+    int playerScore;
+    public Text scoreText;
+
+    // singleton
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    //Update score dan ui
+    public void GetScore(int point)
+    {
+        playerScore += point;
+        scoreText.text = playerScore.ToString();
+    }
+}
